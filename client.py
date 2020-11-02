476896dafa1cab7d65a17adc6dc81dc2eb8b79c5
psteeep <psteeep@users.noreply.github.com>
2020-11-02 19:29:16 +0200

    implemented sockets in class

--- client.py
+++ client.py
@@ -1,34 +1,80 @@
+from socket import socket
 import socket
-import time
-import random
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(("localhost", 5000))
 
-time_duration = float(input("ввдеіть час виділений на виконання = "))
-att = 0
+class StartClient:
 
-list_random = []
-tmp_list = []
+    def __init__(self):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client.connect(("localhost", 5000))
 
-while True:
-    data = client.recv(2048) # наше загадане число
-    ready_to = int(data.decode("utf-8"))
-    print(ready_to)
+    def __del__(self):
+        self.client.close()
 
-    time_start = time.time()
-    while time.time() < time_start + time_duration:
+    def rec(self, param):
+        data = self.client.recv(param)
+        return data
 
-        guessing = random.randint(0, 2000000)
-        list_random.append(guessing)
-        att += 1
-        if guessing == ready_to:
-            print('your number was guessed it is = ', guessing)
-            print("att = ", att)
+    def sendall(self, param):
+        pass
+
+    def recv(self, param):
+        pass
+
+
+def sendable_data(data):
+    return str(data).rjust(1024, " ").encode("utf-8")
+
+
+def binarySearch(data, val):
+    lo, hi = 0, len(data) - 1
+    best_ind = lo
+    while lo <= hi:
+        mid = lo + (hi - lo) // 2
+        if data[mid] < val:
+            lo = mid + 1
+        elif data[mid] > val:
+            hi = mid - 1
+        else:
+            best_ind = mid
             break
+        # check if data[mid] is closer to val than data[best_ind]
+        if abs(data[mid] - val) < abs(data[best_ind] - val):
+            best_ind = mid
+    return best_ind
 
-    else:
-        for i in list_random:
-            tmp_list.append(i - ready_to)
 
-        print("near = ", min(tmp_list) + ready_to)
+def generate_list(range1, range2):
+    list_random = []
+    for i in range(int(range1), int(range2)):
+        f = i + 1
+        list_random.append(f)
+    return list_random.sort()
+
+
+def main():
+    client = StartClient()
+
+    ##########################################
+    lim_one = input("Input range 1 -> ")
+    lim_two = input("Input range 2 -> ")
+
+    client.sendall(sendable_data(lim_one))
+    client.sendall(sendable_data(lim_two))
+
+    ############################################
+
+    data = client.recv(1024)
+
+    ready_to = data.decode()
+    print("ur random numb is", ready_to)
+
+
+    list_random = generate_list(lim_one, lim_two)
+
+    ind = binarySearch(list_random, int(ready_to))
+    print("num is -", ind)
+
+
+if __name__ == "__main__":
+    main()
--- server.py
+++ server.py
@@ -1,14 +1,57 @@
+from socket import socket
 import socket
 import random
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(("localhost", 5000))
-server.listen()
+
+class Start:
+
+        def __init__(self):
+
+            self.connect = None
+            self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.server.bind(("localhost", 5000))
+            self.server.listen()
+            self.num = None
+
+        def connection(self):
+            self.connect, addr = self.server.accept()
+            print(f'{addr} has connected')
+
+        def send_random(self, range1, range2):
+            # data = connect.recv(1024)
+            self.num = random.randint(range1, range2)
+            return self.num
+
+        def rec(self, param):
+            data = self.server.recv(param)
+            return data
+
+        def __del__(self):
+            self.server.close()
+
+        def accept(self):
+            conn, addr = self.server.accept()
+            return conn, addr
 
 
-while True:
-    connect, addr = server.accept()
-    print(f'{addr} has connected')
-    #data = connect.recv(1024)
-    num = random.randint(0,2000000)
-    connect.send(repr(num).encode("utf-8"))
+def main():
+
+    server = Start()
+
+    while True:
+        conn, addr = server.accept()
+        print(f'{addr} has connected')
+        #####################################################
+        data = conn.recv(1024)
+        data2 = conn.recv(1024)
+        f = data.decode("utf-8").strip()
+        g = data2.decode("utf-8").strip()
+
+        ######################################################
+        num = server.send_random(int(f), int(g))
+        conn.sendall(str(num).encode())
+    #######################################################
+
+
+if __name__ == "__main__":
+    main()
